091d6ae097bce62e7d3e6aa2808958b1c01e6244
Michaela Berry <michaelaberry2019@gmail.com>
2019-11-18 20:20:08 +1100

    full Holiday API

--- Project1.js
+++ Project1.js
@@ -28,16 +28,5 @@ $.ajax({url:queryURL,method:'GET'})
     // localStorage.setItem('holidayArray', holidayArrayString);
 });
 
-
-// softcode below
-// var queryparameter = "pretty&country="+countryCode+"&year="+yearValue+"&key="+apikey;
-// var URL = domain + endpoint + "?"+ queryparameter;
-
-
-// Array into object for Harry's Data structure (so Harry's calendar can grab dates)
-
-// var arrOfPubHol = [
-    // {country: "", date: "", name:""},
-// ]
 // us: https://holidayapi.com/v1/holidays?pretty&country=US&year=2018&key=420dcd21-1835-4198-9255-9cd624da6327
 // uk: https://holidayapi.com/v1/holidays?pretty&country=US&year=2018&key=420dcd21-1835-4198-9255-9cd624da6327
