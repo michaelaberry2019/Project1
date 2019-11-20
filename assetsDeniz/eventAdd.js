66c6dc49d3a26e89d3cc3bb1efb74b289711d50b
Deniz Uzel <d.uzel238@gmail.com>
2019-11-20 19:53:42 +1100

    created event modal

--- assetsDeniz/eventAdd.js
+++ assetsDeniz/eventAdd.js
@@ -5,15 +5,16 @@ button.addEventListener("click", MilestoneArray);
 
 function MilestoneArray() {
     // var details = document.getElementById("Milestone-date","Milestone-name","Milestone-description");
-    var details = document.getElementById("Milestone-date");
-    console.log(details.value);
-    // var event = {
-    //     title:,
-    //     startDate:details.value
-    // }
-    MilestoneData.push(details.value);
-    console.log(MilestoneData);
-    var details = JSON.stringify(details);
+    var date = document.getElementById("Milestone-date");
+    console.log(date.value);
+    var name = document.getElementById("Milestone-name");
+    console.log(name.value);
+    var description = document.getElementById("Milestone-description");
+    console.log(description.value);
+    
+    // MilestoneData.push(details.value);
+    // console.log(MilestoneData);
+    // var details = JSON.stringify(details);
     
     
 }
